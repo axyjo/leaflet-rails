b990a3de4e090331c480842dd5d50820224944d4
Jan Graichen <jg@altimos.de>
2013-07-18 17:50:58 +0200

    Add images to precompile option for rails 4.0.
    Rails 4.0 does not precompile non-JS/CSS assets in `vendor`
    anymore. They need to be added to precompile option by the engine.

--- lib/leaflet-rails.rb
+++ lib/leaflet-rails.rb
@@ -3,7 +3,9 @@ require "leaflet-rails/version"
 module Leaflet
   module Rails
     class Engine < ::Rails::Engine
-      # Rails -> use vendor directory.
+      initializer 'leaflet-rails.precompile' do |app|
+        app.config.assets.precompile += %w(layers-2x.png layers.png marker-icon-2x.png marker-icon.png marker-shadow.png)
+      end
     end
   end
 end
