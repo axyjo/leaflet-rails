e40ece488b66753b5a1b8431954d989f769070ab
pezholio <pezholio@gmail.com>
2013-09-19 13:57:55 +0100

    Add helpers

--- lib/leaflet-rails.rb
+++ lib/leaflet-rails.rb
@@ -1,4 +1,5 @@
 require "leaflet-rails/version"
+require "leaflet-rails/view_helpers"
 
 module Leaflet
   mattr_accessor :tile_layer
@@ -9,6 +10,10 @@ module Leaflet
     class Engine < ::Rails::Engine
       initializer 'leaflet-rails.precompile' do |app|
         app.config.assets.precompile += %w(layers-2x.png layers.png marker-icon-2x.png marker-icon.png marker-shadow.png)
+      end
+      
+      initializer 'leaflet-rails.helpers' do
+        ActionView::Base.send :include, Leaflet::ViewHelpers
       end
     end
   end
