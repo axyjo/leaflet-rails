238175682e8aaf6a1a5ac880de42efa0ec6ef599
Akshay Joshi <me@akshayjoshi.com>
2016-04-18 20:41:51 -0400

    Convert specs to RSpec 3.4.4 syntax with Transpec
    This conversion is done by Transpec 3.2.2 with the following command:
        transpec
    
    * 35 conversions
        from: obj.should
          to: expect(obj).to
    
    * 2 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/view_helpers_spec.rb
+++ spec/view_helpers_spec.rb
@@ -14,7 +14,7 @@ describe Leaflet::ViewHelpers do
   end
 
   it 'should mix in view helpers on initialization' do
-    @view.should respond_to(:map)
+    expect(@view).to respond_to(:map)
   end
 
   it 'should set the method configuration options' do
@@ -23,9 +23,9 @@ describe Leaflet::ViewHelpers do
 	      :zoom => 18
 	  	})
 
-	  result.should match(/L.tileLayer\('http:\/\/{s}.somedomain\.com\/blabla\/{z}\/{x}\/{y}\.png'/)
-	  result.should match(/attribution: 'Some attribution statement'/)
-	  result.should match(/maxZoom: 18/)
+	  expect(result).to match(/L.tileLayer\('http:\/\/{s}.somedomain\.com\/blabla\/{z}\/{x}\/{y}\.png'/)
+	  expect(result).to match(/attribution: 'Some attribution statement'/)
+	  expect(result).to match(/maxZoom: 18/)
   end
 
   it 'should generate a basic map with the correct latitude, longitude and zoom' do
@@ -33,7 +33,7 @@ describe Leaflet::ViewHelpers do
 	      :latlng => [51.52238797921441, -0.08366235665359283],
 	      :zoom => 18
 	  	})
-	  result.should match(/map\.setView\(\[51.52238797921441, -0.08366235665359283\], 18\)/)
+	  expect(result).to match(/map\.setView\(\[51.52238797921441, -0.08366235665359283\], 18\)/)
   end
 
   it 'should generate a marker' do
@@ -46,7 +46,7 @@ describe Leaflet::ViewHelpers do
              :latlng => [51.52238797921441, -0.08366235665359283],
            }
         ])
-    result.should match(/marker = L\.marker\(\[51.52238797921441, -0.08366235665359283\]\).addTo\(map\)/)
+    expect(result).to match(/marker = L\.marker\(\[51.52238797921441, -0.08366235665359283\]\).addTo\(map\)/)
   end
 
   it 'should generate a marker with a popup' do
@@ -60,8 +60,8 @@ describe Leaflet::ViewHelpers do
              :popup => "Hello!"
            }
         ])
-    result.should match(/marker = L\.marker\(\[51.52238797921441, -0.08366235665359283\]\).addTo\(map\)/)
-    result.should match(/marker\.bindPopup\('Hello!'\)/)
+    expect(result).to match(/marker = L\.marker\(\[51.52238797921441, -0.08366235665359283\]\).addTo\(map\)/)
+    expect(result).to match(/marker\.bindPopup\('Hello!'\)/)
   end
 
   it 'should generate a marker with a popup and a custom icon' do
@@ -85,9 +85,9 @@ describe Leaflet::ViewHelpers do
          }
      ])
     expected_icon_def = "var #{icon_options[:name]}0 = L.icon({iconUrl: '#{icon_options[:icon_url]}', shadowUrl: '#{icon_options[:shadow_url]}', iconSize: #{icon_options[:icon_size]}, shadowSize: #{icon_options[:shadow_size]}, iconAnchor: #{icon_options[:icon_anchor]}, shadowAnchor: #{icon_options[:shadow_anchor]}, popupAnchor: #{icon_options[:popup_anchor]}})"
-    result.should include(expected_icon_def)
-    result.should match(/marker = L\.marker\(\[51.52238797921441, -0.08366235665359283\], \{icon: house\d+\}\).addTo\(map\)/)
-    result.should match(/marker\.bindPopup\('Hello!'\)/)
+    expect(result).to include(expected_icon_def)
+    expect(result).to match(/marker = L\.marker\(\[51.52238797921441, -0.08366235665359283\], \{icon: house\d+\}\).addTo\(map\)/)
+    expect(result).to match(/marker\.bindPopup\('Hello!'\)/)
   end
 
   it 'should generate many markers with popups and custom icons' do
@@ -117,12 +117,12 @@ describe Leaflet::ViewHelpers do
        ])
     expected_icon_def_1 = "var #{icon_options[:name]}0 = L.icon({iconUrl: '#{icon_options[:icon_url]}', shadowUrl: '#{icon_options[:shadow_url]}', iconSize: #{icon_options[:icon_size]}, shadowSize: #{icon_options[:shadow_size]}, iconAnchor: #{icon_options[:icon_anchor]}, shadowAnchor: #{icon_options[:shadow_anchor]}, popupAnchor: #{icon_options[:popup_anchor]}})"
     expected_icon_def_2 = "var #{icon_options[:name]}1 = L.icon({iconUrl: '#{icon_options[:icon_url]}', shadowUrl: '#{icon_options[:shadow_url]}', iconSize: #{icon_options[:icon_size]}, shadowSize: #{icon_options[:shadow_size]}, iconAnchor: #{icon_options[:icon_anchor]}, shadowAnchor: #{icon_options[:shadow_anchor]}, popupAnchor: #{icon_options[:popup_anchor]}})"
-    result.should include(expected_icon_def_1)
-    result.should include(expected_icon_def_2)
-    result.should match(/marker = L\.marker\(\[51.52238797921441, -0.08366235665359283\], \{icon: house\d+\}\).addTo\(map\)/)
-    result.should match(/marker = L\.marker\(\[51.54238797921441, -0.08566235665359283\], \{icon: house\d+\}\).addTo\(map\)/)
-    result.should match(/marker\.bindPopup\('Hello!'\)/)
-    result.should match(/marker\.bindPopup\('Farewell!'\)/)
+    expect(result).to include(expected_icon_def_1)
+    expect(result).to include(expected_icon_def_2)
+    expect(result).to match(/marker = L\.marker\(\[51.52238797921441, -0.08366235665359283\], \{icon: house\d+\}\).addTo\(map\)/)
+    expect(result).to match(/marker = L\.marker\(\[51.54238797921441, -0.08566235665359283\], \{icon: house\d+\}\).addTo\(map\)/)
+    expect(result).to match(/marker\.bindPopup\('Hello!'\)/)
+    expect(result).to match(/marker\.bindPopup\('Farewell!'\)/)
   end
 
   it 'should have defaults for icon options' do
@@ -139,9 +139,9 @@ describe Leaflet::ViewHelpers do
          }
      ])
     expected_icon_def = "var icon0 = L.icon({iconUrl: '#{icon_options[:icon_url]}', shadowUrl: '', iconSize: [], shadowSize: [], iconAnchor: [0, 0], shadowAnchor: [0, 0], popupAnchor: [0, 0]})"
-    result.should include(expected_icon_def)
-    result.should match(/marker = L\.marker\(\[51.52238797921441, -0.08366235665359283\], \{icon: icon\d+\}\).addTo\(map\)/)
-    result.should match(/marker\.bindPopup\('Hello!'\)/)
+    expect(result).to include(expected_icon_def)
+    expect(result).to match(/marker = L\.marker\(\[51.52238797921441, -0.08366235665359283\], \{icon: icon\d+\}\).addTo\(map\)/)
+    expect(result).to match(/marker\.bindPopup\('Hello!'\)/)
   end
 
   it 'should override the method configuration options if set' do
@@ -154,9 +154,9 @@ describe Leaflet::ViewHelpers do
 	  	:max_zoom => 4
 	  	)
 
-  	  result.should match(/L.tileLayer\('http:\/\/{s}.someotherdomain\.com\/blabla\/{z}\/{x}\/{y}\.png'/)
-  	  result.should match(/attribution: 'Some other attribution text'/)
-  	  result.should match(/maxZoom: 4/)
+  	  expect(result).to match(/L.tileLayer\('http:\/\/{s}.someotherdomain\.com\/blabla\/{z}\/{x}\/{y}\.png'/)
+  	  expect(result).to match(/attribution: 'Some other attribution text'/)
+  	  expect(result).to match(/maxZoom: 4/)
   end
 
   it 'should pass any configuration options to L.tileLayer if set' do
@@ -171,11 +171,11 @@ describe Leaflet::ViewHelpers do
       :key2 => 42
       )
 
-      result.should match(/L.tileLayer\('http:\/\/{s}.someotherdomain\.com\/blabla\/{z}\/{x}\/{y}\.png'/)
-      result.should match(/attribution: 'Some other attribution text'/)
-      result.should match(/maxZoom: 4/)
-      result.should match(/someKey: 'some value'/)
-      result.should match(/key2: '42'/)
+      expect(result).to match(/L.tileLayer\('http:\/\/{s}.someotherdomain\.com\/blabla\/{z}\/{x}\/{y}\.png'/)
+      expect(result).to match(/attribution: 'Some other attribution text'/)
+      expect(result).to match(/maxZoom: 4/)
+      expect(result).to match(/someKey: 'some value'/)
+      expect(result).to match(/key2: '42'/)
 
   end
 
@@ -188,11 +188,11 @@ describe Leaflet::ViewHelpers do
               :latlng => [51.62238797921441, -0.08366235665359284],
                 })
 
-          result.should match(/id=\'first_map'/)
-          result.should match(/L.map\('first_map'/)
+          expect(result).to match(/id=\'first_map'/)
+          expect(result).to match(/L.map\('first_map'/)
 
-          result1.should match(/id=\'second_map'/)
-          result1.should match(/L.map\('second_map'/)
+          expect(result1).to match(/id=\'second_map'/)
+          expect(result1).to match(/L.map\('second_map'/)
 
   end
 
@@ -211,7 +211,7 @@ describe Leaflet::ViewHelpers do
                     :fillOpacity => 0.5
                   }
                   ])
-    result.should match(/L.circle\(\[\'51.52238797921441\', \'-0.08366235665359283\'\], 12, \{
+    expect(result).to match(/L.circle\(\[\'51.52238797921441\', \'-0.08366235665359283\'\], 12, \{
            color: \'red\',
            fillColor: \'#f03\',
            fillOpacity: 0.5
@@ -226,25 +226,25 @@ describe Leaflet::ViewHelpers do
       :no_container => true
       )
 
-    result.should_not match(/<div id='map'>/)
+    expect(result).not_to match(/<div id='map'>/)
   end
 
   it 'should generate a map and add a polyline' do
     result = @view.map(
                 :polylines => [{:latlngs => [[51.5, -0.08], [-51.5, 0.08]], :options => {:color => "green"}}]
                 )
-    result.should match(Regexp.quote('L.polyline([[51.5, -0.08], [-51.5, 0.08]],{"color":"green"}).addTo(map);'))
+    expect(result).to match(Regexp.quote('L.polyline([[51.5, -0.08], [-51.5, 0.08]],{"color":"green"}).addTo(map);'))
   end
 
   it 'should generate a map and add fitbounds' do
     result = @view.map(
                 :fitbounds => [[51.5, -0.08],[-51.5, 0.08]]
                 )
-    result.should match(Regexp.quote("map.fitBounds(L.latLngBounds([[51.5, -0.08], [-51.5, 0.08]]));"))
+    expect(result).to match(Regexp.quote("map.fitBounds(L.latLngBounds([[51.5, -0.08], [-51.5, 0.08]]));"))
   end
 
   it 'should not require a center option to generate a map' do
     result = @view.map({})
-    result.should_not match(Regexp.quote("map.setView"))
+    expect(result).not_to match(Regexp.quote("map.setView"))
   end
 end
